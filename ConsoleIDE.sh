7012136a90802353500fea873d29e1ea2569512c
benProgram <117262113+mcodeZer0@users.noreply.github.com>
2023-01-09 01:44:47 +0800

    Add files via upload

---
+++ ConsoleIDE.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# title
+echo "  /@@@@@@  /@@@@@@ /@@@@@@@  /@@@@@@@@ "
+echo " /@@__  @@|_  @@_/| @@__  @@| @@_____/ "
+echo "| @@  \__/  | @@  | @@  \ @@| @@       "
+echo "| @@ /@@@@#@| @@  | @@  | @@| @@@@@    "
+echo "| @@|______/| @@  | @@  | @@| @@__/    "
+echo "| @@    @@  | @@  | @@  | @@| @@       "
+echo "|  @@@@@@/ /@@@@@@| @@@@@@@/| @@@@@@@@ "
+echo " \______/ |______/|_______/ |________/ "
+
+echo "-----------------------------------------------------"
+# echos
+echo " -- ConsoleIDE Program"
+echo " -- Compiler: java, python, ruby, js, php, c"
+echo " -- Required Installation: compiler"
+echo " -- Java-Version: required jdk-17"
+echo " -- command #create a file: cnsl - create new-file"
+echo " -- command #read a file: cnsl - read-file"
+echo " -- command #delete a file: cnsl - del-source"
+
+# commands
+cd src/main/java/com/program/myEditor
+javac Main.java
+java Main.java
+
